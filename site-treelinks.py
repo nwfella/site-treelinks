4662956259569f41258dd8e02fea839dddc62efc
t-mon <sinewave97013@gmail.com>
2023-02-16 23:47:59 -0800

    Filtering out site tag's to only display valid URL's and mailto's
    Previous version was displaying every designated site tag, now these results are being filtered down to only valid http(s) and mailto: formatted strings.

--- site-treelinks.py
+++ site-treelinks.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
 
 # User inputs URL to scrape
 url = input("Enter a website URL to scrape: ")
@@ -15,11 +16,16 @@ links = []
 for link in soup.find_all("a"):
     href = link.get("href")
     if href is not None:
-        links.append(href)
+        parsed_url = urlparse(href)
+        if parsed_url.scheme and parsed_url.netloc:
+            full_url = href
+        else:
+            full_url = urljoin(url, href)
+        links.append(full_url)
 
 # Print Em and thats a wrap!! ;)
 print("=" * 50)
-print("Links found on the page:")
+print("Valid, full URLs found on the page:")
 print("-" * 50)
 for link in links:
     print(link)
